3fddbd34327c0db17bf2666b793a3547dffd390c
Tirey Morris <tirey@hey.com>
2023-09-20 16:23:19 -0500

    Settled on this combination of tooling. New landing page

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 🚀 everything you need, and nothing you don't. 🌊
 
+Run `bun dev`  and edit `src/index.ts` to get started!
+
 [Bun](https://bun.sh/) provides the runtime, test runner, and package manager, and database via [SQLite](https://bun.sh/docs/api/sqlite)
 
-[Hono](https://hono.dev) is a robust web framework with great DX and performance.
+[Hono](https://hono.dev) is a robust web framework with great DX and performance
 
-[PicoCSS](https://picocss.com/) allows us to write semantic HTML that actually looks good.
+[Tailwind](https://tailwindcss.com/) allows us to keep our styles close to our HTML
 
-[HTMX](https://htmx.org/reference/) and [Hyperscript](https://hyperscript.org/) give us 99% of the client-side interactivity most apps need.
+[HTMX](https://htmx.org/reference/) gives 99% of the client-side interactivity most apps need
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,89 @@
 import * as elements from "typed-html";
 import { Attributes, CustomElementHandler } from "typed-html";
 import { Hono } from "hono";
-import { logger } from 'hono/logger'
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 function Layout({ children }: Attributes) {
   return (
-    <html lang="en">
+    <html lang="en" hx-swap="outerHTML transition:true">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="hyperwave" />
         <title>hyperwave</title>
         <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.classless.min.css"
-        />
-        <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"
         />
         <script src="https://unpkg.com/htmx.org@1.9.5" />
-        <script src="https://unpkg.com/hyperscript.org@0.9.11" />
+        <script src="https://cdn.tailwindcss.com" />
+        <script>htmx.config.globalViewTransitions = true</script>
       </head>
 
-      <body>
-        <main>{children}</main>
+      <body class="bg-blue-100 w-full">
+        <main class="bg-blue-100 sm:p-10 p-4 flex flex-col w-full justify-center gap-8 max-w-6xl">
+          {children}
+        </main>
       </body>
     </html>
   );
 }
 
-app.use('*', logger())
-app.get("/", (c) =>
-  c.html(
+app.use("*", logger());
+app.get("/instructions", ({ html }) =>
+  html(
+    <div class="text-lg bg-blue-200 rounded-sm p-2">
+      <p>
+        <code>$ bun dev</code>
+        <br />
+        <br />
+        then edit <code>src/index.ts</code> to get started!
+      </p>
+    </div>,
+  ),
+);
+app.get("/", ({ html }) =>
+  html(
     <Layout>
-      <p>🚀welcome to Hyperwave! 🌊</p>
-      <p>
-        <a href="https://bun.sh/">Bun</a> provides the runtime, test runner,
-        package manager, and database via{" "}
-        <a href="https://bun.sh/docs/api/sqlite">SQLite</a>
-      </p>
-      <p>
-        <a href="https://hono.dev">Hono</a> is a robust web framework
-        with great DX and performance.
-      </p>
-      <p>
-        <a href="https://picocss.com/">PicoCSS</a> allows us to write semantic
-        HTML that actually looks good.
-      </p>
-      <p>
-        <a href="https://htmx.org/reference/">HTMX</a> and{" "}
-        <a href="https://hyperscript.org/">Hyperscript</a> give us 99% of the
-        client-side interactivity most apps need.
-      </p>
+      <h1 class="text-4xl">welcome to hyperwave 🌊</h1>
+      <div>
+        <button
+          class="bg-blue-200 p-4 text-xl font-bold"
+          hx-get="/instructions"
+          hx-target="closest div"
+        >
+          instructions
+        </button>
+      </div>
+      <ul class="flex flex-col gap-3">
+        <li>
+          <a class="font-bold" href="https://bun.sh/">
+            Bun
+          </a>{" "}
+          provides the runtime, test runner, package manager, and{" "}
+          <a href="https://bun.sh/docs/api/sqlite">SQLite bindings</a>
+        </li>
+        <li>
+          <a class="font-bold" href="https://hono.dev">
+            Hono
+          </a>{" "}
+          is a robust web framework with great DX and performance
+        </li>
+        <li>
+          <a class="font-bold" href="https://tailwindcss.com/">
+            Tailwind
+          </a>{" "}
+          allows us to keep our styles close to our HTML
+        </li>
+        <li>
+          <a class="font-bold" href="https://htmx.org/reference/">
+            HTMX
+          </a>{" "}
+          gives 99% of the client-side interactivity most apps need
+        </li>
+      </ul>
     </Layout>,
   ),
 );

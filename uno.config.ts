6109df02363d595b39bc89114d77463c060c4ebd
Tirey Morris <tirey@hey.com>
2024-09-09 17:29:23 -0600

    Add htmx matchers for unocss

--- uno.config.ts
+++ uno.config.ts
@@ -14,4 +14,32 @@ export default defineConfig({
       },
     }),
   ],
+  variants: [
+    (matcher) => {
+      if (matcher.startsWith("htmx-request:")) {
+        return {
+          matcher: matcher.slice("htmx-request:".length),
+          selector: (s) => `.htmx-request ${s}, ${s}.htmx-request`,
+        };
+      }
+      if (matcher.startsWith("htmx-settling:")) {
+        return {
+          matcher: matcher.slice("htmx-settling:".length),
+          selector: (s) => `.htmx-settling ${s}, ${s}.htmx-settling`,
+        };
+      }
+      if (matcher.startsWith("htmx-swapping:")) {
+        return {
+          matcher: matcher.slice("htmx-swapping:".length),
+          selector: (s) => `.htmx-swapping ${s}, ${s}.htmx-swapping`,
+        };
+      }
+      if (matcher.startsWith("htmx-added:")) {
+        return {
+          matcher: matcher.slice("htmx-added:".length),
+          selector: (s) => `.htmx-added ${s}, ${s}.htmx-added`,
+        };
+      }
+    },
+  ],
 });

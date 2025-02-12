8e4336c543f74c498047ad5083468bea01f6cefc
Tirey Morris <tirey@hey.com>
2025-02-11 20:26:33 -0600

    bump versions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ function Layout({ title, children }: LayoutProps) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌊</text></svg>"
         />
-        <script src="https://unpkg.com/htmx.org@1.9.9"></script>
+        <script src="https://unpkg.com/htmx.org@2.0.0"></script>
         <link rel="stylesheet" href="/styles/uno.css" />
         <script>htmx.config.globalViewTransitions = true</script>
         <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
